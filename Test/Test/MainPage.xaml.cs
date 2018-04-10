3fd83b7180149ba0064a90bf4a6eb1f7040812fc
PracticasSGA <38222828+PracticasSGA@users.noreply.github.com>
2018-04-10 11:08:53 -0500

    v0.0.
    ...

---
+++ Test/Test/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Test.Resources;
+namespace Test
+{
+	public partial class MainPage : ContentPage
+	{
+        private Button btnSaludar;
+        private Entry txtNombres;
+        private MainPage self;
+		public MainPage()
+		{
+			InitializeComponent();
+            this.setElements();
+            this.setListeners();
+		}
+
+        private void setElements()
+        {
+            this.self = this;
+            this.btnSaludar = this.FindByName<Button>("btnSaludar_XAML");
+            this.txtNombres = this.FindByName<Entry>("txtNombres_XAML");
+        }
+
+        private void setListeners()
+        {
+            this.btnSaludar.Clicked += this.btnSaludar_Click;
+        }
+
+        private void btnSaludar_Click(object sender, EventArgs e)
+        {
+
+            String response = Request.post(new Dictionary<string, string> { { "nombre", this.txtNombres.Text } } );
+            DisplayAlert("Alerta", response , "OK");
+        }
+	}
+}
---
+++ Test/Test/Resources/Request.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Test.Resources
+{
+    class Request
+    {
+        private static String web = "http://192.168.1.7/sga/modules/mobile/test.php";
+
+        public static String get(Dictionary<string, string> parameters)
+        {
+            String responseFromServer;
+            WebRequest request = WebRequest.Create(Request.web);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            response.Close();
+
+            return responseFromServer;
+        }
+
+        public static String post(Dictionary<string, string> parameters)
+        {
+            WebRequest request = WebRequest.Create(Request.web);
+            request.Method = "POST";
+            String postData = Request.generateParameters(parameters);
+            Byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            WebResponse response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            String responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            return responseFromServer;
+        }
+
+        private static String generateParameters(Dictionary<string, string> parameters)
+        {
+            if (parameters == null) return ""; 
+            String data = "";
+            foreach (var parameter in parameters)
+            {
+                data += (parameter.Key + "=" + parameter.Value + "&");
+            }
+            data.Remove(data.Length - 1);
+            return data;
+        }
+    }
+}
